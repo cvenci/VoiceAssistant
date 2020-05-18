c2de9ee36d9d2fec8b2d0dcd2c5b2d10e0ad6fda
cvenci <syntaxeror12345@gmail.com>
2020-05-18 06:17:35 +0100

    modifications

--- NLP/src/tokenizer.py
+++ NLP/src/tokenizer.py
@@ -1,12 +1,25 @@
+"""
+Reading the input from the voice reco phase and
+transferring it to a structured XML file ready
+to be parsed and used by the classifier to
+determine the type of the command in the input
+"""
+
 from nltk.tokenize import WordPunctTokenizer
 import xml.etree.cElementTree as ET
+from xml.dom import minidom  # pycharm error
 
 f = open('../data/user_requests/req.txt')
 line = f.readline()
 tokenizer = WordPunctTokenizer()
 tokens = tokenizer.tokenize(line)
 root = ET.Element('root')
+tokElem = ET.SubElement(root, 'tokenization')
+i = 1
 for t in tokens:
-    word = ET.SubElement(root,'word')
-tree = ET.ElementTree(root)
-tree.write('try.xml')
+    word = ET.SubElement(tokElem, 'word', id=str(i), value=str(t))
+    i += 1
+
+xmlstr = minidom.parseString(ET.tostring(root)).toprettyxml(indent="    ")
+with open("../data/user_requests/req.xml", "w", encoding='utf-8') as f:
+    f.write(xmlstr)
