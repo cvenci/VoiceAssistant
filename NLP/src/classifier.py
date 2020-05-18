b211f808b7d76b4a095a3dded80f696394eaab31
cvenci <syntaxeror12345@gmail.com>
2020-05-18 06:18:37 +0100

    modifications

---
+++ NLP/src/classifier.py
@@ -0,0 +1,7 @@
+"""
+Calculating the similarity between the user query
+and the xml description of applications and affect
+the query to an application (as a basic query)
+or to the advanced queries model
+"""
+
